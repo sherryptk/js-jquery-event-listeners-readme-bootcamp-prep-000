77550397bb86fb30bc088550e17ee7229e003d09
Sherry <sherryptk@gmail.com>
2017-09-29 18:50:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('input').on('keydown', function(e) {
+  $('input').on('keydown', function(key) {
     if(key.which == 71){
         alert('g was pressed');
     }
